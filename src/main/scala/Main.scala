0b42309064fc5aa70b859432c36b8a0f0bc39b5d
JohnLilleker <jlilleker@yahoo.co.uk>
2018-01-25 16:36:53 +0000

    More of a menu in the main, can choose an opponent and how many games to play

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,11 +1,25 @@
 object Main {
   def main(args: Array[String]): Unit = {
+    import scala.io.StdIn.readLine
 
-    val name = scala.io.StdIn.readLine("What is your name? ")
-    val winner = Game.play(new SmarterAI(balance = 0.65), new Human(name), 10)
-    println(s"$winner has won!")
+    val game = readLine("Do you want to play (Y) or watch 2 AIs fight (N)? ")
 
-    // AI vs AI
-    //println(Game.play(new SmarterAI("Skynet", 0.35), new SmarterAI("Deep Thought", 0.65), 10000))
+    if (game.head.toUpper == 'Y') {
+      val name = readLine("What is your name? ")
+      val games = readLine("How many wins? ")
+      val ai = readLine("Which AI? 1 : Random, 2 : 0.65 Random, 3 : 0.35 Random ")
+      var comp = new AI
+      if (ai == "2") comp = new SmarterAI(balance = 0.35)
+      if (ai == "3") comp = new SmarterAI(balance = 0.65)
+
+      val winner = Game.play(comp, new Human(name), games.toInt)
+      println(s"$winner has won!")
+    }
+    else {
+      // AI vs AI
+      val games = readLine("How many wins? ")
+      println(Game.play(new SmarterAI("Skynet", 0.35), new SmarterAI("Deep Thought", 0.65), games.toInt))
+    }
+
   }
 }
